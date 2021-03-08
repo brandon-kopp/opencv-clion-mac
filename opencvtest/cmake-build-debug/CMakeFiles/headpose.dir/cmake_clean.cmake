7b5b50b040d292592206de3d9a1b07aa46989452
brandon-kopp <69154564+brandon-kopp@users.noreply.github.com>
2021-03-08 16:30:27 -0500

    Add files via upload

---
+++ opencvtest/cmake-build-debug/CMakeFiles/headpose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/headpose.dir/headpose.cpp.o"
+  "headpose"
+  "headpose.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/headpose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ opencvtest/cmake-build-debug/CMakeFiles/opencvtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencvtest.dir/headpose.cpp.o"
+  "opencvtest"
+  "opencvtest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencvtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
